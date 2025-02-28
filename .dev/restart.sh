28098802d3a5ed38471cac176a584eaa2bd18300
HoomKh <khoshbinhooman@gmail.com>
2025-02-28 22:58:16 +0330

    fix

---
+++ .dev/restart.sh
@@ -0,0 +1,29 @@
+echo "\n\n\n\n------------------------------------\nRunning: sudo git checkout main\n------------------------------------\n\n\n\n"
+sudo git checkout main
+
+echo "\n\n\n\n------------------------------------\nRunning: sudo git pull\n------------------------------------\n\n\n\n"
+sudo git pull
+
+echo "\n\n\n\n------------------------------------\nRunning: cd /projects/hooman_frontend\n------------------------------------\n\n\n\n"
+cd /projects/hooman_frontend
+
+echo "\n\n\n\n------------------------------------\nRunnin: sudo pm2 delete "hooman_frontend"\n------------------------------------\n\n\n\n"
+sudo pm2 delete "hooman_frontend"
+
+echo "\n\n\n\n------------------------------------\nRunnin: sudo pm2 save --force\n------------------------------------\n\n\n\n"
+sudo pm2 save --force
+
+echo "\n\n\n\n------------------------------------\nRunning: sudo npm install --frozen-lockfile\n------------------------------------\n\n\n\n"
+sudo npm install --frozen-lockfile
+
+echo "\n\n\n\n------------------------------------\nRunning: sudo npm run build\n------------------------------------\n\n\n\n"
+sudo npm run build
+
+echo "\n\n\n\n------------------------------------\nRunning: sudo pm2 start "npm start" --name "hooman_frontend"\n------------------------------------\n\n\n\n"
+sudo pm2 start "npm start" --name "hooman_frontend"
+
+echo "\n\n\n\n------------------------------------\nRunning: sudo pm2 save\n------------------------------------\n\n\n\n"
+sudo pm2 save
+
+echo "\n\n\n\n------------------------------------\nRunning: sudo pm2 startup\n------------------------------------\n\n\n\n"
+sudo pm2 startup
